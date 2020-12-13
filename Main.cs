7892fd1382096a3c601871139ffab2a9d1d63264
Jan Eich <j.eich@keiro.ch>
2020-12-13 13:23:30 +0100

    Setup mod loading

---
+++ Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityModManagerNet;
+
+namespace HueRandomizer
+{
+    public static class Main
+    {
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            return true;
+        }
+
+    }
+}
